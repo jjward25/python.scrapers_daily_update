d74cc1691bd35e02434cc9ff5a8b4a4a6c990cfd
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-06-11 09:15:17 -0600

    defense dept

--- Current Events/gov_pkg/cabinet/defense.py
+++ Current Events/gov_pkg/cabinet/defense.py
@@ -8,37 +8,35 @@ import requests
 today = date.today()
 yesterday = date.today() - timedelta(1)
 two_ago = date.today() - timedelta(2)
-three_ago = date.today() - timedelta(3)
 today_word = today.strftime("%B %d, %Y")
 yesterday_word = yesterday.strftime("%B %d, %Y")
 two_ago_word = two_ago.strftime("%B %d, %Y")
-three_ago_word = three_ago.strftime("%B %d, %Y")
 #print(today_word)
 #print(yesterday_word)
 #print(two_ago_word)
-#print(three_ago_word)
-date_list = [today_word,yesterday_word,two_ago_word,three_ago_word]
+date_list = [today_word,yesterday_word,two_ago_word]
 
 ## Headers is used because a User-Agemt was required by the website
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46'}
-link = "https://www.ed.gov/news/press-releases"
+link = "https://www.defense.gov/Newsroom/"
 page = requests.get(link, headers=headers)
 soup = BeautifulSoup(page.content, 'lxml')
 
 ## Actual HTML pull
 object_list = []
-for item in soup.find_all('li'):
+for item in soup.find_all(class_="lside"):
 
     #if item.find('span').get_text() in date_list:
 
-    title = item.find()
-    ilink = item.find()
-    notes = item.find()
-    idate = item.find()
+    title = item.find(class_="title").get_text()
+    ilink = item.find_all('a')[1].get('href')
+    #notes = item.find()
+    #idate = item.find('time').get_text()
 
-    obj_data = {'source':'Ed Dept', 'title': 'title', 'link': 'ilink', 'Notes': '', 'date': 'idate'}
+    obj_data = {'source':'Defense Dept', 'title': title, 'link': ilink, 'Notes': '', 'date': 'idate'}
     object_list.append(obj_data)
 
 ## Final dataframe is defined
-state_dept_df = pd.DataFrame(object_list)
-print(state_dept_df)
+defense_dept_df = pd.DataFrame(object_list)
+defense_dept_df = defense_dept_df.head()
+print(defense_dept_df)
--- Current Events/gov_pkg/gov.py
+++ Current Events/gov_pkg/gov.py
@@ -5,16 +5,21 @@ from datetime import date
 import pandas as pd
 
 # Modules
-from whitehouse import white_house_df
-from supremecourt import supreme_court_df
-from statedept import state_dept_df
-from eddept import ed_dept_df
+from other.whitehouse import white_house_df
+from other.supremecourt import supreme_court_df
+
+from cabinet.state import state_dept_df
+from cabinet.ed import ed_dept_df
+from cabinet.treasury import treasury_dept_df
+from cabinet.defense import defense_dept_df
 
 combined_df = pd.DataFrame()
 combined_df = combined_df.append(white_house_df)
 combined_df = combined_df.append(supreme_court_df)
 combined_df = combined_df.append(state_dept_df)
 combined_df = combined_df.append(ed_dept_df)
+combined_df = combined_df.append(treasury_dept_df)
+combined_df = combined_df.append(defense_dept_df)
 #print(combined_df)
 
 
