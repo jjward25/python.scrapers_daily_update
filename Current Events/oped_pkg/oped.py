2f6c9dfe9198298bb7a3971301fa162fa995d3c6
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-06-11 22:09:10 -0600

    oped

---
+++ Current Events/oped_pkg/oped.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup  ## BeautifulSoup is a web parsing package to help pull specific HTML components
+import urllib.request  ## to get a access to a URL and its content
+import csv
+from datetime import date
+import pandas as pd
+
+# Modules
+from nationalreview import national_review_df
+
+
+combined_df = pd.DataFrame()
+combined_df = combined_df.append(national_review_df)
+
+#print(combined_df)
+
+
+oped_df = combined_df
+print(oped_df)
+oped_df.to_csv(r'C:\Users\Josep\OneDrive\Desktop\Coding\CA Scrapers\Current Events\oped_pkg\oped_data.csv')
