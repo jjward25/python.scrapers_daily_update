c05325ffac261b11b3788afe27357d366530ee3c
jjward25 <45015765+jjward25@users.noreply.github.com>
2021-06-11 19:33:14 -0600

    congress

--- Current Events/gov_pkg/gov.py
+++ Current Events/gov_pkg/gov.py
@@ -7,6 +7,7 @@ import pandas as pd
 # Modules
 from other.whitehouse import white_house_df
 from other.supremecourt import supreme_court_df
+from other.congress import congress_df
 from other.irs import irs_df
 
 from cabinet.agriculture import agriculture_dept_df
@@ -29,6 +30,7 @@ from cabinet.va import va_dept_df
 combined_df = pd.DataFrame()
 combined_df = combined_df.append(white_house_df)
 combined_df = combined_df.append(supreme_court_df)
+combined_df = combined_df.append(congress_df)
 combined_df = combined_df.append(irs_df)
 
 combined_df = combined_df.append(agriculture_dept_df)
--- Current Events/gov_pkg/other/congress.py
+++ Current Events/gov_pkg/other/congress.py
@@ -11,7 +11,7 @@ two_ago = date.today() - timedelta(2)
 today_word = today.strftime("%B %d, %Y")
 yesterday_word = yesterday.strftime("%B %d, %Y")
 two_ago_word = two_ago.strftime("%B %d, %Y")
-today_link = today.strftime("%Y/%m/%d/")
+yesterday_link = yesterday.strftime("%Y-%m-%d")
 #print(today_word)
 #print(yesterday_word)
 #print(two_ago_word)
@@ -19,24 +19,23 @@ date_list = [today_word,yesterday_word,two_ago_word]
 
 ## Headers is used because a User-Agemt was required by the website
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46'}
-link = ""
+link = "https://www.congress.gov/search?q=%7b%22congress%22:%22all%22,%22source%22:%22legislation%22,%22search%22:%22chamberActionDateTotal:\%22"+yesterday_link+"|117|passed\%22+AND+billIsReserved:\%22N\%22%22%7d&pageSort=documentNumber:asc&searchResultViewType=expanded"
 page = requests.get(link, headers=headers)
 soup = BeautifulSoup(page.content, 'lxml')
 
 ## Actual HTML pull
 object_list = []
-for item in soup.find_all(class_=''):
-
+for item in soup.find_all(class_='expanded'):
+    
     #if item.find('span').get_text() in date_list:
+    title = item.find(class_='result-heading').get_text() + item.find(class_='result-title').get_text()
+    ilink = "https://www.congress.gov" + item.find('a').get('href')
+    #notes = item.find()
+    idate = yesterday_word
 
-    title = item.find()
-    ilink = item.find()
-    notes = item.find()
-    idate = item.find()
-
-    obj_data = {'source':'', 'title': '', 'link': '', 'Notes': '', 'date': ''}
+    obj_data = {'source':'', 'title': title , 'link': ilink, 'Notes': '', 'date': idate}
     object_list.append(obj_data)
 
 ## Final dataframe is defined
-_dept_df = pd.DataFrame(object_list)
-print(_dept_df)
+congress_df = pd.DataFrame(object_list)
+print(congress_df)
